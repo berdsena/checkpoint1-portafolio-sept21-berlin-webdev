998a21ff778773dd4edf5a3d6645f7a00d106434
kariakito <pilarmaestre@gmail.com>
2021-10-15 10:55:59 +0200

    algoritmo 2 finish leap year

--- algo/2/isLeapYear.js
+++ algo/2/isLeapYear.js
@@ -20,6 +20,8 @@ Autre rappel: Tu dois utiliser l'opérateur modulo (%) pour vérifier si un nomb
 
 function isLeapYear(year) {
   // TODO
+
+  return ((year % 4 == 0) && (year % 100 != 0)) || (year % 400 == 0);
 }
 
 module.exports = isLeapYear;
