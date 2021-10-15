751f0bc8e9d9ed4dd7bc7f0972d221ffc4965511
Ugo <ugotheallet@gmail.com>
2021-10-15 13:49:30 +0200

    last push

--- algo/4/findAdults.js
+++ algo/4/findAdults.js
@@ -37,7 +37,9 @@ function findAdults(arr) {
  //persons.map(function(person){
  let male = persons.filter((person) => (person.age > 18 || person.sex === "male"))
  let female = persons.filter((person)=>(person.age > 18 || person.sex === "female"))
- return [[male],[female]]
+ let result = []
+ result.push(male,female)
+ return result
 }
 
 
